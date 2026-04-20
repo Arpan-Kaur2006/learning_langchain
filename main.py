2009135c8fe1cae5abec10524effc3afc7414019
Arpan-Kaur2006 <gs3267306@gmail.com>
2026-04-20 19:32:36 +0530

    how to handle output?

--- main.py
+++ main.py
@@ -1,41 +1,84 @@
+
+
 # from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_core.prompts import PromptTemplate
+# from dotenv import load_dotenv
+
+# from langchain_core.output_parsers import JsonOutputParser            #CONVERT that JSON → Python dict
 # import os
 
+# load_dotenv()
+
+# # create model
 # llm = ChatGoogleGenerativeAI(
 #     model="gemini-2.5-flash",
 #     google_api_key=os.getenv("GOOGLE_API_KEY")
 # )
 
-# response = llm.invoke("Explain AI in 2 lines")
+# # create prompt
+# prompt = PromptTemplate.from_template(
+#     """
+#     Explain {topic} in:
+#     - 2 bullet points
+#     - simple language
+#     """
+# )
+
+# # chain
+# chain = prompt | llm
+
+# # run
+# response = chain.invoke({"topic": "dogs"})
+
+# # output
 # print(response.content)
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser               #CONVERT that JSON → Python dict
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-# create model
+# 1. LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-# create prompt
-prompt = PromptTemplate.from_template(
-    """
-    Explain {topic} in:
-    - 2 bullet points
-    - simple language
-    """
+# 2. Parser
+parser = JsonOutputParser()            
+
+# 3. Prompt      #FORCE model to output JSON (using prompt)
+
+
+prompt = PromptTemplate(
+    template="""
+You are a helpful assistant.
+
+Explain {topic} simply.
+
+Format your response EXACTLY as JSON:
+
+{{
+  "topic_name": "...",
+  "facts": ["...", "..."],                    
+  "fun_fact": "..."
+}}
+
+Do not write anything outside JSON.
+""",
+    input_variables=["topic"]
 )
 
-# chain
-chain = prompt | llm
+# 4. Chain
+chain = prompt | llm | parser
 
-# run
-response = chain.invoke({"topic": "dogs"})
+# 5. Run
+response = chain.invoke({"topic": "Quantum Computing"})
 
-# output
-print(response.content)
+# 6. Output
+print(type(response))
+print(response)
+print(response["facts"])
